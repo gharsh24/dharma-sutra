0f96fb8390e99faa92f0f9176aacb53efed1eb05
Harsh Goyal <harsh.sg360@gmail.com>
2023-06-19 22:56:18 +0530

    first commit

---
+++ src/stories.jsx
@@ -0,0 +1,20 @@
+const stories=[
+    {
+        title:"The four yugas",
+        content:"The four yugas refer to cosmic ages that are used in the Vedic system for measuring universal time. Each yuga represents a different era in human civilization, characterized by specific qualities and attributes. The four yugas, in chronological order, are: \
+\
+        Satya Yuga (Age of Truth):\
+        Satya Yuga is considered the most virtuous and spiritually advanced age. People in this era are believed to be highly ethical, honest, and filled with wisdom. Truth and righteousness prevail, and the divine presence is felt strongly. Human life spans are very long, and there is harmony and abundance in nature. It spans over 1,728,000 years.\
+        \
+        Treta Yuga (Age of Ritual):\
+        Treta Yuga is characterized by a slight decline in moral and spiritual values compared to Satya Yuga. Rituals and sacrifices become more prominent as a means of connecting with the divine. People are still righteous, but human life spans gradually decrease. Knowledge and spirituality are preserved but require more effort to attain. It spans over 1,296,000 years.\
+        \
+        Dvapara Yuga (Age of Dualities):\
+        Dvapara Yuga sees a further decline in moral and spiritual values. People become more attached to material possessions and personal desires. Society becomes more divided, and the practice of rituals becomes more complex. Spiritual knowledge is preserved but starts to fade.It spans over 864,000 years.\
+        \
+        Kali Yuga (Age of Darkness):\
+        Kali Yuga is the current age we are believed to be living in. It is characterized by a significant decline in moral and spiritual values. People become more ego-driven, selfish, and disconnected from their true nature. The pursuit of material gain takes precedence over spiritual growth. However, it is believed that in this age, one can attain spiritual liberation through devotion and sincere effort. It spans over 432,000 years. 5,124 years have passed since Kaliyuga began and 426,876 years still remaining for its end.",
+        url:"https://www.rudraksha-ratna.com/uploads/files/9302571160.jpg",
+        author:"Harsh Goyal"
+    }
+]
